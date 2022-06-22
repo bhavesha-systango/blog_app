bd559c7c45447336b88d44cfc884a939276c797c
bhavesha-systango <bagnihotri@isystango.com>
2022-06-22 12:52:21 +0530

    basic django project

--- articles/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def article_list(request):
-    return render(request, article_list.html)
--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,4 @@ urlpatterns = [
     url(r'admin/', admin.site.urls),
     url(r'^about/$', views.about),
     url(r'^$', views.homepage),
-    url(r'^articles/',include('articles.urls')),
-
 ]
